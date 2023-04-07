e24e34654d7d6ca28318a95d0bbc2580a55c271e
Lorenzo <lorenzo.delsignore94@gmail.com>
2023-04-07 12:49:16 +0200

    Refactor code

--- Introductory Problems/missing_number/missing_number.py
+++ Introductory Problems/missing_number/missing_number.py
@@ -21,31 +21,31 @@ def algorithm2(first_line, second_line):
 
 
 def algorithm3(first_line, second_line):
-    first_line = int(first_line)
-    all_numbers = (first_line * (first_line + 1)) // 2
+    max_number = int(first_line)
+    sum_all_numbers = (max_number * (max_number + 1)) // 2
     numbers = [int(i) for i in second_line.split()]
 
-    missing_number = all_numbers - sum(numbers)
+    missing_number = sum_all_numbers - sum(numbers)
 
     return missing_number
 
 
 def algorithm4(first_line, second_line):
-    first_line = int(first_line)
-    all_numbers = (first_line * (first_line + 1)) // 2
+    max_number = int(first_line)
+    sum_all_numbers = (max_number * (max_number + 1)) // 2
     numbers = map(int, second_line.split())
 
-    missing_number = all_numbers - sum(numbers)
+    missing_number = sum_all_numbers - sum(numbers)
 
     return missing_number
 
 
 def algorithm5(first_line, second_line):
-    first_line = int(first_line)
-    all_numbers = (first_line * (first_line + 1)) // 2
+    max_number = int(first_line)
+    sum_all_numbers = (max_number * (max_number + 1)) // 2
     numbers = sum(int(i) for i in second_line.split())
 
-    missing_number = all_numbers - numbers
+    missing_number = sum_all_numbers - numbers
 
     return missing_number
 
@@ -55,17 +55,7 @@ if __name__ == "__main__":
         first_line = file.readline()
         second_line = file.readline()
 
-    t = timeit.repeat(lambda: algorithm(first_line, second_line), number=1, repeat=50)
-    print(mean(t))
-
-    t = timeit.repeat(lambda: algorithm2(first_line, second_line), number=1, repeat=50)
-    print(mean(t))
-
-    t = timeit.repeat(lambda: algorithm3(first_line, second_line), number=1, repeat=50)
-    print(mean(t))
-
-    t = timeit.repeat(lambda: algorithm4(first_line, second_line), number=1, repeat=50)
-    print(mean(t))
-
-    t = timeit.repeat(lambda: algorithm5(first_line, second_line), number=1, repeat=50)
-    print(mean(t))
+    algorithms = [algorithm, algorithm2, algorithm3, algorithm4, algorithm5]
+    for algo in algorithms:
+        t = timeit.repeat(lambda: algo(first_line, second_line), number=1, repeat=50)
+        print(mean(t))
